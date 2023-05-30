ff5965e350c6e58072e11111f05f7a0798d47e0a
jvholanda2 <jvholanda18@gmail.com>
2023-05-30 14:25:41 -0300

    Cria um script para instalação do VSCode, Arduino IDE, Git, em um ambiente Raspberry Pi OS

---
+++ enviroment.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#Atualizando 
+sudo apt-get update
+sudo apt-get upgrade
+
+#Instalando VSCode
+
+#echo "Iniciando a instalação do VSCode"
+#sudo rm -Rf /opt/vscode/*
+#sudo rm -Rf /opt/vscode/code
+#sudo rm -Rf /usr/share/applications/vscode.desktop
+#sudo apt update
+#sudo apt install code
+#code --version
+#echo "VSCode Instalado"
+
+#Instalando Arduino IDE
+
+#echo "Iniciando a instalação do Arduino IDE"
+#sudo apt install arduino
+#arduino --version
+#echo "Arduino IDE Instalado"
+
+#Instalando Docker****
+
+#Instalando Samba
+
+#Instalando Git
+
+#echo "Iniciando a instalação do Git"
+#sudo apt update
+#sudo apt install git
+#git --version
+#echo "Git Instalado"
+
+echo "Instalações finalizadas. Seu ambiente está pronto!"
+
